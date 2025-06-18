f09ad6ac77fb545c718452ef907e8aba86700820
Ricky <richardhob@gmail.com>
2025-06-17 19:14:43 -0700

    Add initial version of files I use

---
+++ downloader.py
@@ -0,0 +1,69 @@
+
+import os
+import pathlib
+import argparse
+import subprocess
+
+def do(title, indicies, link):
+    idx = ','.join(map(str, indicies))
+    p = subprocess.run(f"yt-dlp -I {idx} -o \"{title}E%(playlist_index)03d.old.%(ext)s\" {link}", shell=True)
+
+def get_rename(name, extension, indicies):
+    values = []
+    for i, index in enumerate(indicies, 1):
+        old = f"{name}E{index:03d}.old.{extension}"
+        new = f"{name}E{i:03d}.{extension}"
+        values.append((old, new))
+
+    return values
+
+def rename(values):
+    for old, new in values:
+        os.rename(old, new)
+
+def fix(name, extension, indicies):
+    values = []
+    for index in indicies:
+        old = f"{name}E{index:04d}.old.{extension}"
+        new = f"{name}E{index:03d}.old.{extension}"
+        values.append((old, new))
+
+    return values
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("name", help="File Name Prefix")
+    parser.add_argument("link", help="Link to the YT Playlist")
+    parser.add_argument("--path", help="Path to the directory to save things to", default=pathlib.Path("."), type=pathlib.Path)
+    parser.add_argument("--renameOnly", help="Skip downloading, rename only", action="store_true", default=False)
+    parser.add_argument("--extension", help='Video file extension (of download file for renaming)', default='mp4')
+    parser.add_argument("--fix", help='Quick fix for movies (Remove)', default=False, action='store_true')
+    parser.add_argument("-x", help='Force the rename', action="store_true", default=False)
+    parser.add_argument("indicies", help="YT Playlist indicies to download", nargs="+", type=int)
+
+    args = parser.parse_args()
+
+    if args.fix:
+        values = fix(args.name, args.extension, args.indicies)
+        if args.x == True:
+            rename(values)
+        else:
+            for (old, new) in values:
+                print(f"{old} -> {new}")
+
+        return
+
+    if args.renameOnly == False:
+        do(args.name, args.indicies, args.link)
+
+    values = get_rename(args.name, args.extension, args.indicies)
+
+    if args.x == True:
+        rename(values)
+    else:
+        for (old, new) in values:
+            print(f"{old} -> {new}")
+
+if __name__ == '__main__':
+    main()
---
+++ rename2.py
@@ -0,0 +1,38 @@
+
+import argparse 
+from pathlib import Path
+
+import natsort as ns
+
+def rename(path, name, season, extension, make_change=False, start_value=1):
+    found = []
+    from_to = {}
+    for some_file in path.iterdir():
+        if some_file.suffix != extension:
+            continue
+        found.append(some_file)
+
+    found = ns.natsorted(found)
+
+    count = start_value
+    for some_file in found:
+        target = Path(path) / f"{name}_S{season:02}E{count:03}{extension}"
+
+        if make_change:
+            some_file.rename(target)
+        else:
+            print(f"{some_file} -> {target}")
+        count += 1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("path")
+    parser.add_argument("name")
+    parser.add_argument("season", type=int)
+    parser.add_argument("-s", type=int, default=1)
+    parser.add_argument("-x", action="store_true")
+    parser.add_argument("--extension", default='.m4v')
+
+    args = parser.parse_args()
+    rename(Path(args.path), args.name, args.season, args.extension, args.x, args.s)
